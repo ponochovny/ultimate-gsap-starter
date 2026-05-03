50c0a373f61957800171e72b8a0dd0ca9730ffa1
ponochovny <ponochovny@gmail.com>
2026-05-03 08:32:23 +0300

    update: vite.config paths

--- vite.config.ts
+++ vite.config.ts
@@ -2,23 +2,30 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
+const base =
+	process.env.NODE_ENV === 'production' ? '/ultimate-gsap-starter/' : '/'
+// const fullPath =
+// 	process.env.NODE_ENV === 'production'
+// 		? __dirname + '/ultimate-gsap-starter/'
+// 		: __dirname
+
 export default defineConfig({
 	plugins: [tailwindcss()],
-	base: '/ultimate-gsap-starter/',
+	base: base,
 	build: {
 		rollupOptions: {
 			input: {
-				main: resolve(__dirname, 'index.html'),
-				animation1: resolve(__dirname, 'src/pages/animation1/index.html'),
-				animation2: resolve(__dirname, 'src/pages/animation2/index.html'),
-				animation3: resolve(__dirname, 'src/pages/animation3/index.html'),
-				animation4: resolve(__dirname, 'src/pages/animation4/index.html'),
-				animation5: resolve(__dirname, 'src/pages/animation5/index.html'),
-				animation6: resolve(__dirname, 'src/pages/animation6/index.html'),
-				animation7: resolve(__dirname, 'src/pages/animation7/index.html'),
-				animation8: resolve(__dirname, 'src/pages/animation8/index.html'),
-				animation9: resolve(__dirname, 'src/pages/animation9/index.html'),
-				animation10: resolve(__dirname, 'src/pages/animation10/index.html'),
+				main: resolve(__dirname, './index.html'),
+				animation1: resolve(__dirname, './src/pages/animation1/index.html'),
+				animation2: resolve(__dirname, './src/pages/animation2/index.html'),
+				animation3: resolve(__dirname, './src/pages/animation3/index.html'),
+				animation4: resolve(__dirname, './src/pages/animation4/index.html'),
+				animation5: resolve(__dirname, './src/pages/animation5/index.html'),
+				animation6: resolve(__dirname, './src/pages/animation6/index.html'),
+				animation7: resolve(__dirname, './src/pages/animation7/index.html'),
+				animation8: resolve(__dirname, './src/pages/animation8/index.html'),
+				animation9: resolve(__dirname, './src/pages/animation9/index.html'),
+				animation10: resolve(__dirname, './src/pages/animation10/index.html'),
 			},
 		},
 	},
